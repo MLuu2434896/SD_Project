552a94094c6cb1e62f3a490c4bee2011ade591c2
amaanmullacodes <amaanaka99@gmail.com>
2023-06-18 19:07:40 -0500

    Maybe fixed map error?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,27 @@ import axios from 'axios';
 import { useState, useEffect } from "react";
 
 function App() {
-  // useState accepts a return type and returns a var in an object and a setter
-  const [ object, setObject ] = useState( [] );
+  const [object, setObject] = useState([]);
 
-  // Test function to see how frontend fetches data from backend
-  useEffect( () => {
-    axios.get( "http://localhost:8000" )
-      .then( (response) => {
-        const data = response.data
-        setObject( data ) 
+  useEffect(() => {
+    axios.get("http://localhost:8000")
+      .then((response) => {
+        const data = response.data;
+        setObject(data);
       })
-      .catch( (e) => {
-        setObject( e )
-      })
-  }, [] );
+      .catch((error) => {
+        setObject(error);
+      });
+  }, []);
 
-  // Test function to see how frontend fetches info from backend
   const print_info = (value, idx) => {
-    return <p key={idx}> {idx + 1}. {value.f_name} {value.l_name} </p>
-  }
+    return <p key={idx}>{idx + 1}. {value.f_name} {value.l_name}</p>;
+  };
 
   return (
     <div className="App">
-      <h1>
-          FastAPI React
-      </h1>
-      { object.map( print_info ) }
+      <h1>FastAPI React</h1>
+      {object.length > 0 ? object.map(print_info) : <p>Loading...</p>}
     </div>
   );
 }
