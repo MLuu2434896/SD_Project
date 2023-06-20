d290bcf7fad6ab8c004da24980d0761905774d59
Man Luu <!Nandemonaiya123>
2023-06-20 01:55:23 -0500

    added a couple of pages along with some functionalities to the homescreen

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "../styles/login.css";
+import { sidebarItems } from "../components/sidebar_items";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+const Login_page = () => {
+    const [ isOpen, setIsOpen ] = useState( false );
+
+    const nagivate = useNavigate();
+
+    const ShowItems = ( item ) =>
+    {
+        const [ showLink, setShowLink ] = useState( false );
+        
+
+        return (
+            <ul> 
+                <li 
+                    className="sidebar_items" 
+                    onMouseEnter={ () => { setShowLink( true ) } }
+                    onMouseLeave={ () => { setShowLink( false ) } } 
+                    onClick={ () => { nagivate( `${item.link}` ) } }> 
+                    {item.item_name} { showLink && <> { item.link } </> }
+                </li>
+            </ul>
+            
+        );
+    };
+
+    return (
+        <div className="LoginPage"> 
+            <div 
+            className={ `sidebar_container ${ isOpen ? "sidebar_on" : "sidebar_off" } ` }>
+                <div className="sidebar_header"> Sidebar </div>
+                <div className="sidebar_list"> { sidebarItems.map( ShowItems ) } </div>
+            </div>
+            <button className="sidebar_toggle_btn" 
+                    onClick={ () => { setIsOpen( !isOpen ) } } >
+                    Toggle 
+            </button>
+        </div>
+        
+    )
+};
+export default Login_page;
