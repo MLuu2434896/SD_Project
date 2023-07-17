5569a0cb8e08f0ae21df2a3811dfd2753327b34d
John M <jmaru06251@outlook.com>
2023-07-17 12:50:30 -0500

    Revert "Removed role on Register page"
    This reverts commit 9b4dfc39a46744bc3fb4ea2bc80ff62a50e04a02.

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -9,6 +9,7 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [firstname, setFirstName] = useState("");
     const [lastname, setLastName] = useState("");
+    const [role, setRole] = useState("");
     const [password, setPassword] = useState("");
 
     const [errorMessage, setErrorMessage] = useState("");
@@ -18,7 +19,7 @@ const Register = () => {
         const requestOptions = {
             method: "POST",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({ email: email, first_name: firstname, last_name: lastname, hashed_password: password})
+            body: JSON.stringify({ email: email, first_name: firstname, last_name: lastname, role: role, hashed_password: password})
         };
 
             const response = await fetch("http://localhost:8000/api/create_user", requestOptions);                      
@@ -67,6 +68,13 @@ const Register = () => {
                 </div>
 
                 <div className="field">
+                    <label className="label">Role:</label>
+                    <div className="control">
+                        <input type="text" placeholder="Enter your role" value = {role} onChange ={(e) => setRole(e.target.value)} className = "input_box" required></input>
+                    </div>
+                </div>
+
+                <div className="field">
                     <label className="label">Password:</label>
                     <div className="control">
                         <input type="password" placeholder="Enter password" value = {password} onChange ={(e) => setPassword(e.target.value)} className = "input_box" required></input>
@@ -89,7 +97,9 @@ const Register = () => {
             {firstname}
             <br />
             {lastname}
-            <br />           
+            <br />
+            {role}
+            <br />
             {password} 
         </div>
     )
