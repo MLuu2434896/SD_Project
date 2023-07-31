3c9bd544a0592059420c3a919884199bb26071f3
HDamra02 <88245624+HDamra02@users.noreply.github.com>
2023-07-31 03:44:59 -0500

    Revert "Add files via upload"
    This reverts commit 2b88ee856f73ef51d5fab24671acf6e623b978af.

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,11 +2,12 @@ import sqlalchemy as SQL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from constants import SQLALCHEMY_DATABASE_URL
+
+# Path to store the database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 # Create the database 
 engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False} )
-
 SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine )
 
 Base = declarative_base()
--- backend/src/models.py
+++ backend/src/models.py
@@ -9,43 +9,25 @@ class Employee( DB.Base ):
     __tablename__ = "employees"
     id = SQL.Column( SQL.Integer, primary_key=True, index=True )
     email = SQL.Column( SQL.String, unique=True, index=True )
-    hashed_password = SQL.Column( SQL.BINARY )
+    hashed_password = SQL.Column( SQL.String )
     is_active = SQL.Column( SQL.Boolean, default=True )
-    first_name = SQL.Column( SQL.String, index=True )
-    last_name = SQL.Column( SQL.String, index=True )
-    role = SQL.Column( SQL.Integer, index=True, default=0 )
+    first_name = SQL.Column( SQL.String )
+    last_name = SQL.Column( SQL.String )
 
     # This will create a bidirectional relationship with other tables
     # specified in the parameters
-    supervisor = ORM.relationship( "Supervisor", back_populates="employees" )
+    supervisor = ORM.relationship( "Supervisor", back_populates="employee" )
 
 class Supervisor( DB.Base ):
     __tablename__ = "supervisors"
     id = SQL.Column( SQL.Integer, primary_key=True, index=True )
     employee_id = SQL.Column( SQL.Integer, SQL.ForeignKey( "employees.id" ) )
+    email = SQL.Column( SQL.String, unique=True )
+    first_name = SQL.Column( SQL.String )
+    last_name = SQL.Column( SQL.String )
     date_created = SQL.Column( SQL.DateTime, default=datetime.now() )
-    first_name = SQL.Column( SQL.String, index=True )
-    last_name = SQL.Column( SQL.String, index=True )
-    email = SQL.Column( SQL.String, index=True )
 
-    # This will create a bidirectional relationship with 'Employee' table
-    employees = ORM.relationship( "Employee", back_populates="supervisor" )
-
-class Sprint( DB.Base ):
-    __tablename__ = "sprints"
-    sprint_id = SQL.Column( SQL.Integer, primary_key=True, index=True )
-    sprint_name = SQL.Column( SQL.String )
-    start_date = SQL.Column( SQL.DateTime, default=datetime.now() )
-    #end_date = SQL.Column( SQL.DateTime, default=datetime.now() )
-
-    employee_id = SQL.Column( SQL.Integer, SQL.ForeignKey( "employees.id" ) )
-
-class Task( DB.Base ):
-    __tablename__ = "tasks"
-    task_id = SQL.Column( SQL.Integer, primary_key=True, index=True )
-    task_name = SQL.Column( SQL.String )
-    task_info = SQL.Column( SQL.String )
-    is_complete = SQL.Column( SQL.Integer, default=0 )
-
-    sprint_id = SQL.Column( SQL.Integer, SQL.ForeignKey( "sprints.sprint_id" ) )
+    # This will create a bidirectional relationship with other tables
+    # specified in the parameters
+    employees = ORM.relationship( "Employee", back_populates="supervisors" )
 
